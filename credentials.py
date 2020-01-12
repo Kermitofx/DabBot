8e0c3a57caaab8eb715b2493041873b90066afc9
Kermitofx <39018448+Kermitofx@users.noreply.github.com>
2020-01-12 19:57:44 -0300

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 
 ENGINE = create_engine('sqlite:///music.db')
-TOKEN = 'TELEGRAM BOT API KEY'
+TOKEN = '928105941:AAG4k-VU6jKVL8VtMZeMSWFw5IFtKLAsj3g'
 CELULAR = 1 # 1 enabled 0 disabled
 IPOD = 0
 Ipod_Path = "/media/vaniture/ipod" #example 
